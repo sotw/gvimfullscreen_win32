4d2041c1f5a98c66ae51b673e492ce8fd0ac6150
sotw <workthethird@gmail.com>
2014-04-26 16:46:37 +0800

    change path to vim73

--- copyover.bat
+++ copyover.bat
@@ -1,3 +1,4 @@
 nmake
 del "\program files (x86)\vim\vim72\gvimfullscreen.dll"
-copy gvimfullscreen.dll "\program files (x86)\vim\vim72"
+copy gvimfullscreen.dll "\program files (x86)\vim\vim73"
+pause
